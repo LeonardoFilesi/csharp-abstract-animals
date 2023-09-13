54b69dccd91b2142985de5256d67204539b40964
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-13 16:36:41 +0200

    esercizio 1 corretto

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -6,41 +6,41 @@
         {
 
             Dog dog = new Dog();
-            dog.DoCall();
-            dog.Sleep();
-            dog.WhatDoUEat();
-            
+           
             Sparrow sparrow = new Sparrow();
-            sparrow.DoCall();
-            sparrow.Sleep();
-            sparrow.WhatDoUEat();
-            
+          
             Eagle eagle = new Eagle();
-            eagle.DoCall();
-            eagle.Sleep();
-            eagle.WhatDoUEat();
             
             Dolphin dolphin = new Dolphin();
-            dolphin.DoCall();
-            dolphin.Sleep(); 
-            dolphin.WhatDoUEat();
 
             Console.WriteLine("Here is a list of animals and some of their characteristics:");
-            Console.WriteLine($"This is a 'dog', the sound that it makes is: {dog.DoCall}");
-            Console.WriteLine($"The 'dog' diet is: {dog.WhatDoUEat}");
-            Console.WriteLine($"And when a 'dog' sleeps it just {dog.Sleep}");
+            Console.WriteLine("This is a 'dog', the sound that it makes is:");
+            dog.DoCall();
+            Console.WriteLine("The 'dog' diet is:");
+            dog.WhatDoUEat();
+            Console.WriteLine("And when a 'dog' sleeps it just");
+            dog.Sleep();
 
-            Console.WriteLine($"This is a 'sparrow', the sound that it makes is: {sparrow.DoCall}");
-            Console.WriteLine($"The 'sparrow' diet is: {sparrow.WhatDoUEat}");
-            Console.WriteLine($"And when a 'sparrow' sleeps it just {sparrow.Sleep}");
+            Console.WriteLine("This is a 'sparrow', the sound that it makes is:");
+            sparrow.DoCall();
+            Console.WriteLine("The 'sparrow' diet is:");
+            sparrow.WhatDoUEat();
+            Console.WriteLine("And when a 'sparrow' sleeps it just");
+            sparrow.Sleep();
 
-            Console.WriteLine($"This is a 'eagle', the sound that it makes is: {eagle.DoCall}");
-            Console.WriteLine($"The 'eagle' diet is: {eagle.WhatDoUEat}");
-            Console.WriteLine($"And when a 'eagle' sleeps it just {eagle.Sleep}");
+            Console.WriteLine("This is a 'eagle', the sound that it makes is:");
+            eagle.DoCall();
+            Console.WriteLine("The 'eagle' diet is:");
+            eagle.WhatDoUEat();
+            Console.WriteLine("And when a 'eagle' sleeps it just");
+            eagle.Sleep();
 
-            Console.WriteLine($"This is a 'dolphin', the sound that it makes is: {dolphin.DoCall}");
-            Console.WriteLine($"The 'dolphin' diet is: {dolphin.WhatDoUEat}");
-            Console.WriteLine($"And when a 'dolphin' sleeps it just {dolphin.Sleep}");
+            Console.WriteLine("This is a 'dolphin', the sound that it makes is:");
+            dolphin.DoCall();
+            Console.WriteLine("The 'dolphin' diet is:");
+            dolphin.WhatDoUEat();
+            Console.WriteLine("And when a 'dolphin' sleeps it just");
+            dolphin.Sleep();
         }
     }
 }
